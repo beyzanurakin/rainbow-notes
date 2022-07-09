74946dde8539b1135c9600a899ff2cbdf67e4c1f
beyzanurakin <akinbeyzanur@gmail.com>
2022-07-09 14:29:30 +0300

    Add new Components for better design and functionality

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>
+        Rainbow Notes <span className='material-symbols-outlined'>looks</span>
+      </h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Search.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { searchNotes } from '../redux/notes/notesSlice'
+
+function Search() {
+  const [query, setQuery] = useState('')
+  const dispatch = useDispatch()
+  const handleSearch = (e) => {
+    console.log(query)
+    e.preventDefault()
+    dispatch(searchNotes(query))
+    setQuery('')
+  }
+  return (
+    <div className='search'>
+      <form onSubmit={handleSearch}>
+        <input
+          type='text'
+          placeholder='Search...'
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </form>
+      <button>clear search</button>
+    </div>
+  )
+}
+
+export default Search
